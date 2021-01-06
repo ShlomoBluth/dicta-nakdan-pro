176f6ab4b465d7ed1d3b77d593e1908e128b7ee8
Shlomo Bluth <lomibluth@gmail.com>
2021-01-06 07:35:04 +0200

    add commit

--- cypress/integration/requestsTest.js
+++ cypress/integration/requestsTest.js
@@ -9,35 +9,23 @@ describe('RequestsTests',()=>{
     cy.visit('https://nakdanpro.dicta.org.il/')
   })
 
-
-  it('Successful request with 60 seconds delay of response when clicking the run butten of'+
-  ' nakdan pro page '
-  ,()=>{
-    cy.nakdanProRequest({
-      delaySeconds:60
+  it('Message after request failed with 2 minutes delay of response when clicking the ron butten'+
+    ' of nakdan page'
+    ,()=>{
+        cy.nakdanRequest({
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+          delaySeconds:60*2
+        })
     })
-  })
-
-  it('Message after request failed with 61 seconds delay of response when clicking the ron butten'+
-  ' of nakdan pro page'
-  ,()=>{
-    cy.nakdanProRequest({
-      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-      delaySeconds:61
-      })
-  })
 
   
-  it('Message after request failed with status code 500 when clicking the ron butten of nakdan '+
-  'pro page'
-  ,()=>{
-    cy.nakdanProRequest({
-      status:500,
-      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+   it('Message after request failed with status code 500 when clicking the ron butten of nakdan page'
+    ,()=>{
+        cy.nakdanRequest({
+          status:500,
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+        })
     })
-  })
-
   
-   
-    
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,25 +13,22 @@ Cypress.Commands.add('nakdanProRequest',({status=200,message='',delaySeconds=0})
   ).as('genreclassify')
   cy.get('a[class="welcome-close-link"]').click({force: true})
   cy.get('textarea[placeholder="הזן טקסט כאן"]').type('משה קיבל תורה')
-    if(message.length>0){
-      cy.contains(message).should('not.exist')
-    }
-    cy.get('div[class="run-button"]').within(()=>{
-        cy.get('button').click({force: true})
-    })
-    if(delaySeconds>0){
-      cy.get('[class*="spinner"').should('exist')
-    }
-    cy.wait('@api',{responseTimeout:1000*delaySeconds})
-    if(message.length>0){
-      cy.wait('@genreclassify',{responseTimeout:1000*delaySeconds}).then(xhr=>{
-        cy.get('[class*="spinner"').should('not.exist')
-          cy.wrap(xhr.response.statusCode).should('eq', status).then(()=>{
-              cy.contains(message).should('exist')
-          })
-      })
-    }else{
-      cy.get('@api').its('response.body').should('eq','it worked!')
-    }
+  if(message.length>0){
+    cy.contains(message).should('not.exist')
+  }
+  cy.get('div[class="run-button"]').within(()=>{
+      cy.get('button').click({force: true})
+  })
+  if(delaySeconds>0){
+    cy.get('[class*="spinner"').should('exist')
+  }
+  cy.wait('@api',{responseTimeout:1000*delaySeconds})
+  cy.wait('@genreclassify',{responseTimeout:1000*delaySeconds})
+  cy.get('[class*="spinner"').should('not.exist')
+  cy.get('@api').its('response.statusCode').should('eq',status)
+
+  if(message.length>0){
+    cy.contains(message).should('exist')
+  }
       
   })  
