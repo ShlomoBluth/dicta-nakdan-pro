0c9f98f68066beb4b97a2bb0c4f483f4a9194d96
Shlomo Bluth <lomibluth@gmail.com>
2021-01-15 09:27:16 +0200

    RequestsTests

--- cypress/integration/requestsTest.js
+++ cypress/integration/requestsTest.js
@@ -9,18 +9,39 @@ describe('RequestsTests',()=>{
     cy.visit('https://nakdanpro.dicta.org.il/')
   })
 
-  it('Error message for response with a delay of 2 minutes when clicking the run butten'+
+  it('Error message for genreclassify response with a delay of 2 minutes when clicking the run butten'+
   ' of nakdanpro page',()=>{
     cy.nakdanProRequest({
+      url:'/genreclassify',
       message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
       delaySeconds:60*2
     })
   })
 
-  
-  it('Error message for response with status code 500 when clicking the run butten of nakdanpro page',
-  ()=>{
+  it('Error message for api response with a delay of 2 minutes when clicking the run butten'+
+  ' of nakdanpro page',()=>{
     cy.nakdanProRequest({
+      url:'/api',
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+      delaySeconds:60*2
+    })
+  })
+
+
+  
+  it('Error message for genreclassify response with status code 500 when clicking the run butten'+
+  ' of nakdanpro page',()=>{
+    cy.nakdanProRequest({
+      url:'/genreclassify',
+      status:500,
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+    })
+  })
+
+  it('Error message for api response with status code 500 when clicking the run butten'+
+  ' of nakdanpro page',()=>{
+    cy.nakdanProRequest({
+      url:'/api',
       status:500,
       message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
     })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,8 @@
-Cypress.Commands.add('nakdanProRequest',({status=200,message='',delaySeconds=0})=>{
-  cy.intercept( '/api', {
+Cypress.Commands.add('nakdanProRequest',({url,status=200,message='',delaySeconds=0})=>{
+  cy.intercept('POST', '/api', {
     delayMs:1000*delaySeconds,
-    body:'it worked!',
     statusCode: status
-  },
-  ).as('api')
+  },)
   cy.get('a[class="welcome-close-link"]').click({force: true})
   cy.get('textarea[placeholder="הזן טקסט כאן"]').type('משה קיבל תורה')
   if(message.length>0){
@@ -15,7 +13,7 @@ Cypress.Commands.add('nakdanProRequest',({status=200,message='',delaySeconds=0})
   })
 
   if(delaySeconds>0){
-    cy.get('[class*="spinner"]',{timeout:1000*delaySeconds}).should('not.exist')
+    cy.get(/[class*="spinner"]|[class*="loader"]/g,{timeout:1000*delaySeconds}).should('not.exist')
   }
   
   if(message.length>0){
